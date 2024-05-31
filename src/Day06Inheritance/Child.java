c0bddfb864619632950c7bb97200ba5b191d878b
MdSaifulAkhand <s.i.palash2013@gmail.com>
2024-05-31 18:37:56 -0400

    Commit1

---
+++ src/Day06Inheritance/Child.java
@@ -0,0 +1,9 @@
+package Day06Inheritance;
+
+public class Child {
+	
+	Child(){
+		System.out.println("Hello Child");
+	}
+
+}
---
+++ src/Day06Inheritance/Tester.java
@@ -0,0 +1,11 @@
+package Day06Inheritance;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		Child childOb = new Child();
+
+	}
+
+}
